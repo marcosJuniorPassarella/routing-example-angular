ac04aed7109e3cd757e5aacd6d1cd26221fed8f2
MJWORKS <mjworks2020@outlook.com>
2022-06-30 16:46:13 -0300

    Added app routing module file

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,58 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { EditServerComponent } from "./servers/edit-server/edit-server.component";
+import { ServerComponent } from "./servers/server/server.component";
+import { ServersComponent } from "./servers/servers.component";
+import { UserComponent } from "./users/user/user.component";
+import { UsersComponent } from "./users/users.component";
+
+const appRoutes: Routes = [
+  {
+    path: "",
+    component: HomeComponent,
+  },
+  {
+    path: "users",
+    component: UsersComponent,
+    children: [
+      {
+        path: ":id/:name",
+        component: UserComponent,
+      },
+    ],
+  },
+  {
+    path: "servers",
+    component: ServersComponent,
+    children: [
+      {
+        path: ":id",
+        component: ServerComponent,
+      },
+      {
+        path: ":id/edit",
+        component: EditServerComponent,
+      },
+    ],
+  },
+  {
+    path: "not-found",
+    component: PageNotFoundComponent,
+  },
+  // ** captura todos os caminhos que não temos em nossas rotas
+  // essa rota deve ser sempre a última rota da aplicação
+  // senão qualquer caminho mesmo existente irá redirecionar para o not-found
+  {
+    path: "**",
+    redirectTo: "/not-found",
+    pathMatch: "full",
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,48 +12,7 @@ import { EditServerComponent } from "./servers/edit-server/edit-server.component
 import { ServerComponent } from "./servers/server/server.component";
 import { ServersService } from "./servers/servers.service";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
-
-const appRoutes: Routes = [
-  {
-    path: "",
-    component: HomeComponent,
-  },
-  {
-    path: "users",
-    component: UsersComponent,
-    children: [
-      {
-        path: ":id/:name",
-        component: UserComponent,
-      },
-    ],
-  },
-  {
-    path: "servers",
-    component: ServersComponent,
-    children: [
-      {
-        path: ":id",
-        component: ServerComponent,
-      },
-      {
-        path: ":id/edit",
-        component: EditServerComponent,
-      },
-    ],
-  },
-  {
-    path: "not-found",
-    component: PageNotFoundComponent,
-  },
-  // ** captura todos os caminhos que não temos em nossas rotas
-  // essa rota deve ser sempre a última rota da aplicação
-  // senão qualquer caminho mesmo existente irá redirecionar para o not-found
-  {
-    path: "**",
-    redirectTo: "/not-found",
-  },
-];
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -66,7 +25,7 @@ const appRoutes: Routes = [
     ServerComponent,
     PageNotFoundComponent,
   ],
-  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [ServersService],
   bootstrap: [AppComponent],
 })
