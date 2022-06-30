2894c7dd5fb98a8b7b176e7d1e6d1a419c4c15f3
MJWORKS <mjworks2020@outlook.com>
2022-06-30 09:44:08 -0300

    Getting queryParams and fragment

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,27 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { ServersService } from '../servers.service';
+import { ServersService } from "../servers.service";
+import { ActivatedRoute } from "@angular/router";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-edit-server',
-  templateUrl: './edit-server.component.html',
-  styleUrls: ['./edit-server.component.css']
+  selector: "app-edit-server",
+  templateUrl: "./edit-server.component.html",
+  styleUrls: ["./edit-server.component.css"],
 })
 export class EditServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
-  serverName = '';
-  serverStatus = '';
+  server: { id: number; name: string; status: string };
+  serverName = "";
+  serverStatus = "";
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
+    this.getQueryParams();
+    this.getFragment();
   }
 
   onUpdateServer() {
-    this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+    this.serversService.updateServer(this.server.id, {
+      name: this.serverName,
+      status: this.serverStatus,
+    });
   }
 
+  getQueryParams(): void {
+    this.route.queryParams.subscribe({
+      next(value) {
+        console.log(value);
+      },
+    });
+  }
+
+  getFragment(): void {
+    this.route.fragment.subscribe({
+      next(value) {
+        console.log(value);
+      },
+    });
+  }
 }
