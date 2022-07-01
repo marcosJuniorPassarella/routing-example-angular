49966378792d44612ff7c1834fcae8aaf9c1c465
MJWORKS <mjworks2020@outlook.com>
2022-07-01 09:27:50 -0300

    Created auth guard service

---
+++ src/app/auth-guard.service.ts
@@ -0,0 +1,14 @@
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+  UrlTree,
+} from "@angular/router";
+import { Observable } from "rxjs";
+
+export class AuthGuardService implements CanActivate {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | {};
+}
